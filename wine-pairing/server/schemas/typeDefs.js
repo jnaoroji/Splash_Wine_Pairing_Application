67ce833aea8c09a2cfbdd4a8a1872876299a8e4e
jnaoroji <jennynaoroji@gmail.com>
2023-08-14 21:19:48 +1000

    updated typeDefs

--- wine-pairing/server/schemas/typeDefs.js
+++ wine-pairing/server/schemas/typeDefs.js
@@ -9,6 +9,12 @@ const typeDefs = gql`
     thoughts: [Thought]!
   }
 
+  type Search {
+    _id: ID
+    searchText: String
+    createdAt: String
+  }
+
   type Thought {
     _id: ID
     thoughtText: String
@@ -33,15 +39,19 @@ const typeDefs = gql`
     users: [User]
     user(username: String!): User
     thoughts(username: String): [Thought]
+    searches(username: String): [Search]
     thought(thoughtId: ID!): Thought
+    search(searchId: ID!): Search
     me: User
   }
 
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
+    addSearch(searchText: String!): Search
     addThought(thoughtText: String!): Thought
     addComment(thoughtId: ID!, commentText: String!): Thought
+    removeSearch(searchId: ID!): Search
     removeThought(thoughtId: ID!): Thought
     removeComment(thoughtId: ID!, commentId: ID!): Thought
   }
