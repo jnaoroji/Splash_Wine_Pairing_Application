cb9fe85b4d46fea1afbd9e6587a1a8ac62e1f222
jnaoroji <jennynaoroji@gmail.com>
2023-08-14 15:20:03 +1000

    changed navbar and colours + initial file structure

--- wine-pairing/client/src/components/Header/index.js
+++ wine-pairing/client/src/components/Header/index.js
@@ -3,20 +3,25 @@ import { Link } from 'react-router-dom';
 
 import Auth from '../../utils/auth';
 
+
 const Header = () => {
   const logout = (event) => {
     event.preventDefault();
     Auth.logout();
+
+    
   };
   return (
     <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <div>
           <Link className="text-light" to="/">
-            <h1 className="m-0">Tech Thoughts</h1>
+            <h1 className="m-0">Splash</h1>
           </Link>
-          <p className="m-0">Get into the mind of a programmer.</p>
+          <p className="m-0">Every recipe calls for a glass of wine</p>
         </div>
+
+
         <div>
           {Auth.loggedIn() ? (
             <>
--- wine-pairing/client/src/pages/Home.js
+++ wine-pairing/client/src/pages/Home.js
@@ -13,13 +13,14 @@ const Home = () => {
   return (
     <main>
       <div className="flex-row justify-center">
-        <div
+
+        {/* <div
           className="col-12 col-md-10 mb-3 p-3"
           style={{ border: '1px dotted #1a1a1a' }}
         >
           <ThoughtForm />
-        </div>
-        <div className="col-12 col-md-8 mb-3">
+        </div> */}
+        {/* <div className="col-12 col-md-8 mb-3">
           {loading ? (
             <div>Loading...</div>
           ) : (
@@ -28,7 +29,7 @@ const Home = () => {
               title="Some Feed for Thought(s)..."
             />
           )}
-        </div>
+        </div> */}
       </div>
     </main>
   );
